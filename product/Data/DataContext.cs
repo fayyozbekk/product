00fee006bb5077c8f157a693e9172ea226675be1
fayyozbekk <fayyozbekk@gmail.com>
2022-11-01 19:58:32 +0500

    Added DB contexts

---
+++ product/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace product.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
